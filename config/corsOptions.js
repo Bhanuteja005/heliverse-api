be8d8bb8aa6a4197a3f2df47413b1ec244ba103c
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-08-13 13:38:51 +0530

    api

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,12 @@
+// config/corsOptions.js
 const allowedOrigins = [
-  "https://heliverse-steel.vercel.app/",
-  "http://localhost:3000",
+  "https://heliverse-steel.vercel.app",
+  "http://localhost:3000", 
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (
-      allowedOrigins.indexOf(origin) !== -1
-       || !origin
-    ) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not Allowed by CORS"));
